b2ca5c6998efd773be2358cdc65f24c65799b98b
AhmedS624 <assassinscool625@gmail.com>
2022-11-13 10:39:30 +0200

    first draft

---
+++ puplic/init-firebase.js
@@ -0,0 +1,12 @@
+ var firebaseConfig = {
+    apiKey: "AIzaSyD1R9ib5hFrp0aDOcFjFcjzj3y4cLPX_aw",
+    authDomain: "first-project-8acdb.firebaseapp.com",
+    projectId: "first-project-8acdb",
+    storageBucket: "first-project-8acdb.appspot.com",
+    messagingSenderId: "207147251035",
+    appId: "1:207147251035:web:aa06fe29ba8bb3f8fb567e",
+    measurementId: "G-X1D57ZJCBS"
+  };
+
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
